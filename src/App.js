a510e09c3fe7909cfe09f4a734834bbef7883c41
estherkang14 <estherkang14@gmail.com>
2021-02-26 09:58:08 -0500

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      Hello!
+      <Nav />
+      <Main />
+      <Footer />
     </div>
   );
 }
