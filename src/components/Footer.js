ac29971b339578b9f8960d1f45f9241fa44d293c
estherkang14 <estherkang14@gmail.com>
2021-02-28 12:40:42 -0500

    Prep components

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    
+    return (
+        <>
+        <h1>This is the Footer component.</h1>
+        </>
+    )
+    
+}
+
+export default Footer;
---
+++ src/components/Nav.js
@@ -0,0 +1,11 @@
+function Nav() {
+    
+    return (
+        <>
+        <h1>This is the NavBar component.</h1>
+        </>
+    )
+    
+}
+
+export default Nav;
