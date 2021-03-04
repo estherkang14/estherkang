4f51186670a10621945984b2007a1b335fd2ec7e
estherkang14 <estherkang14@gmail.com>
2021-03-04 11:05:06 -0500

    Move logo images into src folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
-
+import logo from '../imgs/eklogo2.png'
 function Nav() {
     
     return (
         <>
         <div className="nav">
-            <img src='../../public/eklogo2.png'/>
+            <img src={logo}/>
             
             <h2>This is an H2</h2>
         </div>
