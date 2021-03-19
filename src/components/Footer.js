a65da38da88654c7078d4b74acb37b337eda1ca5
estherkang14 <estherkang14@gmail.com>
2021-03-19 11:55:36 -0400

    Open footer link to new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,7 +10,8 @@ function Footer() {
         <>
         <div className="footer">
             <div id='footerlinks'>
-                <span class="footerlink"><FontAwesomeIcon icon={faGithub} size="2x" /></span>
+                {/* <span class="footerlink"><FontAwesomeIcon icon={faGithub} size="2x" /></span> */}
+                <a class="footerlink" href="https://github.com/estherkang14" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
                 <span class="footerlink"><FontAwesomeIcon icon={faLinkedin} size="2x" /></span>
                 <span class="footerlink"><FontAwesomeIcon icon={faMedium} size="2x" /></span>
                 <span class="footerlink"><FontAwesomeIcon icon={faEnvelope} size="2x" /></span>
