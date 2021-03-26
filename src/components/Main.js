6b9a1c961809619b154597633b4071e84a8fb8c3
estherkang14 <estherkang14@gmail.com>
2021-03-26 10:58:24 -0400

    remove fragment

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,11 +3,9 @@
 function Main() {
     
     return (
-        <>
         <div className="main">
             <h1 className="main_h1">hi. my name is esther kang.</h1>
         </div>
-        </>
     )
     
 }
