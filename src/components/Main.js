1558bc9d357c9b494d8bd32e904c6b7ed6240b11
estherkang14 <estherkang14@gmail.com>
2021-03-26 12:06:57 -0400

    Commented out basic about info

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,14 +8,14 @@ function Main() {
                 <h1 className="main_hi">hi. </h1> <h1 className="main_name"> my name is esther kang.</h1>
             </div>
 
-            <div className='main_about'>
+            {/* <div className='main_about'>
                 I'm a: 
                     - Korean American, born in New Jersey and raised in Maryland
                     - Software Engineer / Full Stack Web Developer
                     - Bootcamp graduate from Flatiron School '20 - #2commit2git
                     - Dancer
                     - Food enthusiast - I could eat sushi and all types of noodles for days
-            </div>
+            </div> */}
         </div>
     )
     
