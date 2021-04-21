83a36899258dc01293d0ca9a28e9d99dce8dd0d5
Esther Kang <estherkang14@gmail.com>
2021-04-20 22:36:28 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Nav from './components/Nav';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
+// just testing to see if this works lol 
+
 function App() {
   return (
     <>
