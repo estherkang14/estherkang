fcde02e01afed0adee9a197c84bb368a4ecd7f48
Esther Kang <estherkang14@gmail.com>
2021-05-12 16:42:55 +0900

    fixed initial load kind of

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,32 +3,35 @@
 function Main() {
     
     return (
-        <div className="main">
-            <div className="main_intro">
-                <h1 className="main_hi">hi! ☺</h1> <h1 className="main_name"> my name is esther kang.</h1>
+        <section className="main">
+            <div className="main_container grid">
+                <div className="main_intro">
+                    <h1 className="main_hi">hi! ☺</h1> <h1 className="main_name"> my name is esther kang.</h1>
+                    <a href=".main_about" className="down_arrow"><h2>v</h2></a>
+                </div>
+
+                <div className='main_about'>
+                    <h1>about me</h1>
+                    <h2>
+                    I'm a: <br />
+                        - Korean American, born and raised on the East Coast <br />
+                        - Software Engineer / Full Stack Web Developer <br />
+                        - Bootcamp graduate from Flatiron School '20 - #2commit2git <br />
+                        - Dancer <br />
+                        - Food enthusiast - I could eat sushi and all types of noodles for days <br />
+                        - Adventure seeker - love to travel! Currently in Seoul, South Korea <br />
+                        - extra text <br /> </h2>
+                </div>
+
+                {/* <div className='main_portfolio'>
+                    Portfolio will go here
+                </div> */}
+
+                {/* <div className='main_resume'>
+                    Either Resume pdf will go here or a link to my resume
+                </div> */}
             </div>
-
-            <div className='main_about'>
-                <h1>about me</h1>
-                <h2>
-                I'm a: <br />
-                    - Korean American, born and raised on the East Coast <br />
-                    - Software Engineer / Full Stack Web Developer <br />
-                    - Bootcamp graduate from Flatiron School '20 - #2commit2git <br />
-                    - Dancer <br />
-                    - Food enthusiast - I could eat sushi and all types of noodles for days <br />
-                    - Adventure seeker - love to travel! Currently in Seoul, South Korea <br />
-                    - extra text <br /> </h2>
-            </div>
-
-            {/* <div className='main_portfolio'>
-                Portfolio will go here
-            </div> */}
-
-            {/* <div className='main_resume'>
-                Either Resume pdf will go here or a link to my resume
-            </div> */}
-        </div>
+        </section>
     )
     
 }
